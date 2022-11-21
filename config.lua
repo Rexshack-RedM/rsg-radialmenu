f62c1874a68b88053ab69c43caf58e1247862257
RexShack <rexshackuk@gmail.com>
2022-11-21 20:56:25 +0000

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,18 +3,18 @@ Config = {}
 Config.EnableExtraMenu = false
 
 Config.MenuItems = {
-    [1] = {
-        id = 'world',
-        title = 'World',
-        icon = 'globe',
-        items = {
-        },
-    }, 
-    [2] = {
-        id = 'horse',
-        title = 'Horse',
-        icon = 'horse-head',
-        items = {
+	[1] = {
+		id = 'world',
+		title = 'World',
+		icon = 'globe',
+		items = {
+		},
+	}, 
+	[2] = {
+		id = 'horse',
+		title = 'Horse',
+		icon = 'horse-head',
+		items = {
 			{
 				id = 'horselantern',
 				title = 'Horse Lantern',
@@ -23,13 +23,13 @@ Config.MenuItems = {
 				event = 'rsg-stable:client:equipHorseLantern',
 				shouldClose = true
 			},
-        },
-    },    
-    [3] = {
-        id = 'user',
-        title = 'User',
-        icon = 'user',
-        items = {
+		},
+	},
+	[3] = {
+		id = 'user',
+		title = 'User',
+		icon = 'user',
+		items = {
 			{ 	
 				id = 'walkstyles',
 				title = 'Walk Styles',
@@ -164,99 +164,99 @@ Config.MenuItems = {
 				event = 'sellcontraband',
 				shouldClose = true
 			},
-        },
-    },
+		},
+	},
 }
 
 Config.JobInteractions = {
-    ["ambulance"] = {
-        {
-            id = 'statuscheck',
-            title = 'Check Health Status',
-            icon = 'heart-pulse',
-            type = 'client',
-            event = 'hospital:client:CheckStatus',
-            shouldClose = true
-        },{
-            id = 'revivep',
-            title = 'Revive',
-            icon = 'user-doctor',
-            type = 'client',
-            event = 'hospital:client:RevivePlayer',
-            shouldClose = true
-        },{
-            id = 'treatwounds',
-            title = 'Heal wounds',
-            icon = 'bandage',
-            type = 'client',
-            event = 'hospital:client:TreatWounds',
-            shouldClose = true
-        }, {
-            id = 'escort',
-            title = 'Escort',
-            icon = 'user-group',
-            type = 'client',
-            event = 'police:client:EscortPlayer',
-            shouldClose = true
-        }
-    },
-    ["police"] = {
-                {
-                    id = 'handcuff',
-                    title = 'Cuff',
-                    icon = 'user-lock',
-                    type = 'client',
-                    event = 'police:client:CuffPlayerSoft',
-                    shouldClose = true
-                }, {                    
-                    id = 'statuscheck',
-                    title = 'Check Health Status',
-                    icon = 'heart-pulse',
-                    type = 'client',
-                    event = 'hospital:client:CheckStatus',
-                    shouldClose = true
-                }, {
-                    id = 'checkstatus',
-                    title = 'Check status',
-                    icon = 'question',
-                    type = 'client',
-                    event = 'police:client:CheckStatus',
-                    shouldClose = true
-                },{
-                    id = 'revivep',
-                    title = 'Revive',
-                    icon = 'user-doctor',
-                    type = 'client',
-                    event = 'hospital:client:RevivePlayer',
-                    shouldClose = true
-                },{
-                    id = 'escort',
-                    title = 'Escort',
-                    icon = 'user-group',
-                    type = 'client',
-                    event = 'police:client:EscortPlayer',
-                    shouldClose = true
-                }, {
-                    id = 'searchplayer',
-                    title = 'Search',
-                    icon = 'magnifying-glass',
-                    type = 'client',
-                    event = 'police:client:SearchPlayer',
-                    shouldClose = true
-                }, {
-                    id = 'jailplayer',
-                    title = 'Jail',
-                    icon = 'user-lock',
-                    type = 'client',
-                    event = 'police:client:JailPlayer',
-                    shouldClose = true
-                }, {
-                    id = 'lawbadge',
-                    title = 'Badge On/Off',
-                    icon = 'id-badge',
-                    type = 'command',
-                    event = 'lawbadge',
-                    shouldClose = true
-                }, 
-    },
+	["ambulance"] = {
+		{
+			id = 'statuscheck',
+			title = 'Check Health Status',
+			icon = 'heart-pulse',
+			type = 'client',
+			event = 'hospital:client:CheckStatus',
+			shouldClose = true
+		},{
+			id = 'revivep',
+			title = 'Revive',
+			icon = 'user-doctor',
+			type = 'client',
+			event = 'hospital:client:RevivePlayer',
+			shouldClose = true
+		},{
+			id = 'treatwounds',
+			title = 'Heal wounds',
+			icon = 'bandage',
+			type = 'client',
+			event = 'hospital:client:TreatWounds',
+			shouldClose = true
+		}, {
+			id = 'escort',
+			title = 'Escort',
+			icon = 'user-group',
+			type = 'client',
+			event = 'police:client:EscortPlayer',
+			shouldClose = true
+		}
+	},
+	["police"] = {
+		{
+			id = 'handcuff',
+			title = 'Cuff',
+			icon = 'user-lock',
+			type = 'client',
+			event = 'police:client:CuffPlayerSoft',
+			shouldClose = true
+		}, {                    
+			id = 'statuscheck',
+			title = 'Check Health Status',
+			icon = 'heart-pulse',
+			type = 'client',
+			event = 'hospital:client:CheckStatus',
+			shouldClose = true
+		}, {
+			id = 'checkstatus',
+			title = 'Check status',
+			icon = 'question',
+			type = 'client',
+			event = 'police:client:CheckStatus',
+			shouldClose = true
+		},{
+			id = 'revivep',
+			title = 'Revive',
+			icon = 'user-doctor',
+			type = 'client',
+			event = 'hospital:client:RevivePlayer',
+			shouldClose = true
+		},{
+			id = 'escort',
+			title = 'Escort',
+			icon = 'user-group',
+			type = 'client',
+			event = 'police:client:EscortPlayer',
+			shouldClose = true
+		}, {
+			id = 'searchplayer',
+			title = 'Search',
+			icon = 'magnifying-glass',
+			type = 'client',
+			event = 'police:client:SearchPlayer',
+			shouldClose = true
+		}, {
+			id = 'jailplayer',
+			title = 'Jail',
+			icon = 'user-lock',
+			type = 'client',
+			event = 'police:client:JailPlayer',
+			shouldClose = true
+		}, {
+			id = 'lawbadge',
+			title = 'Badge On/Off',
+			icon = 'id-badge',
+			type = 'command',
+			event = 'lawbadge',
+			shouldClose = true
+		}, 
+	},
 }
